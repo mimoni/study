6cb3dac16d7b25b4100d9f5d20925aa560e0a4df
Eugene Parshukov <ev.klit@gmail.com>
2021-04-28 02:22:09 +0300

    add arrays solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,39 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_elem = quick_sort(array).last
+        array.map do |elem|
+          if elem.positive?
+            max_elem
+          else
+            elem
+          end
+        end
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, from = 0, to = nil)
+        to ||= array.size - 1
+
+        return -1 if from > to
+
+        mid = (from + to) / 2
+        return mid if query == array[mid]
+
+        if query < array[mid]
+          search(array, query, from, mid - 1)
+        else
+          search(array, query, mid + 1, to)
+        end
+      end
+
+      def quick_sort(array)
+        return array if array.size < 2
+
+        pivot, *ar = array
+        less = ar.filter { |elem| elem < pivot }
+        greater = ar.filter { |elem| elem >= pivot }
+
+        quick_sort(less) + [pivot] + quick_sort(greater)
       end
     end
   end
